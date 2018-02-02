fd66dbbdbe17ceea149d3e1156def72c2b85404c
johnny <czh1012@outlook.com>
2018-02-02 22:51:15 +0800

    update the pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,27 +1,31 @@
-stage 'prepare the pipeline'
 node() {
 	
-    git poll: false, changelog: false, url: pipelineRepo, credentialsId: "git-credentials", branch: pipelineBranch
+	stage('prepare the pipeline') {
+		
+		git poll: false, changelog: false, url: pipelineRepo, credentialsId: "git-credentials", branch: pipelineBranch
 
-    utilities = load "lib/utilities.groovy"
-    cloudfoundry = load "lib/cloudfoundry.groovy"
-    shell = load "lib/shell.groovy"
-    git = load "lib/git.groovy"
-    pom = load "lib/pom.groovy"
+	    utilities = load "lib/utilities.groovy"
+	    cloudfoundry = load "lib/cloudfoundry.groovy"
+	    shell = load "lib/shell.groovy"
+	    git = load "lib/git.groovy"
+	    pom = load "lib/pom.groovy"
+		
+	}
+	
+	stage('maven package') {
+		
+		git url: repositoryUrl, credentialsId: "git-credentials", branch: branch
+	    artifactId = pom.artifactId(pwd() + "/pom.xml")
+	    version = pom.version(pwd() + "/pom.xml")
+	    majorVersion = pom.majorVersion(pwd() + "/pom.xml")
+	    uniqueVersion = version + "." + utilities.timestamp()
+	    appName = "${artifactId}-${version}".replace(".", "-")
+	    artifactPath = "target/${artifactId}-${uniqueVersion}.jar"
+	    commitId = shell.pipe("git rev-parse HEAD")
+	    bat "mvn -DnewVersion=${uniqueVersion} versions:set"
+	    bat "mvn package"
+	    stash includes: "${artifactPath}", name: 'app-artifact'
+		
+	}
     
-}
-
-stage 'maven build'
-node() {
-    git url: repositoryUrl, credentialsId: "git-credentials", branch: branch
-    artifactId = pom.artifactId(pwd() + "/pom.xml")
-    version = pom.version(pwd() + "/pom.xml")
-    majorVersion = pom.majorVersion(pwd() + "/pom.xml")
-    uniqueVersion = version + "." + utilities.timestamp()
-    appName = "${artifactId}-${version}".replace(".", "-")
-    artifactPath = "target/${artifactId}-${uniqueVersion}.jar"
-    commitId = shell.pipe("git rev-parse HEAD")
-    bat "mvn -DnewVersion=${uniqueVersion} versions:set"
-    bat "mvn package"
-    stash includes: "${artifactPath}", name: 'app-artifact'
 }
